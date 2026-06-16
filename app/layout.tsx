21c9bc25ea71130b082edfe22ed07117771291a8
jana-94 <Janaagilesoftlabs@gmail.com>
2026-06-16 13:02:59 +0530

    gtag update

--- app/layout.tsx
+++ app/layout.tsx
@@ -46,25 +46,28 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en" className={`${inter.variable} ${poppins.variable}`} suppressHydrationWarning>
       <head>
-        {/* Google tag (gtag.js) */}
+        {/* Google Tag Manager */}
         <Script
-          id="gtag-src"
-          src="https://www.googletagmanager.com/gtag/js?id=G-QD1D1BCRPV"
-          strategy="afterInteractive"
-        />
-        <Script
-          id="gtag-init"
+          id="gtm-script"
           strategy="afterInteractive"
           dangerouslySetInnerHTML={{
-            __html: `window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
-gtag('config', 'G-QD1D1BCRPV');`,
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-MWZ9Z3MG');`,
           }}
         />
-        {/* End Google tag (gtag.js) */}
+        {/* End Google Tag Manager */}
       </head>
       <body className="font-[var(--font-inter)]">
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-MWZ9Z3MG"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+
         <LanguageProvider>
           <ThemeProvider
             attribute="class"
